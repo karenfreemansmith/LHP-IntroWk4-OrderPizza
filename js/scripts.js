2678194affb6af0fe0b3a1ebd6634f5bf4bf6edc
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-27 10:32:30 -0700

    create input form and business object with size and topping info

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+//business logic
+function PizzaSizes() {
+  "personal":[4, 2.99],
+  "mini":[6, 4.99],
+  "small":[8, 5.99],
+  "medium":[10, 8.99],
+  "large":[12, 10.99],
+  "xlarge":[16, 15.99],
+  "colossal":[36, 32.99]
+}
+function Toppings() {
+  "peperoni":[.99],
+  "sausage":[.99],
+  "bacon":[.99],
+  "canadian":[.99],
+  "steak":[2.99],
+  "chicken":[1.99],
+  "cheese":[0],
+  "xtra":[.49],
+  "asiago":[2.99],
+  "feta":[2.99],
+  "nacho":[.99],
+  "jalepeno":[.49],
+  "olives":[.49],
+  "pepper":[.49],
+  "redpepper":[.49],
+  "onions":[.49],
+  "tomatoes":[.49],
+  "mushrooms":[.49],
+  "anchovies":[.49]
+}
